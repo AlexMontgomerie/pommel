d5ab6e94ed1280e4db5662aa5a94b71fdffa78ab
AlexMontgomerie <AlexMontgomerie@github.com>
2021-08-26 00:40:15 +0100

    cleaning repo

--- pommel/__init__.py
+++
@@ -1,6 +0,0 @@
-from .plot import plot
-from .report import report
-from .network import network
-from .accelerator import accelerator
-from .memory import memory
-from .featuremap import featuremap
